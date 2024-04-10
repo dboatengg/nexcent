752aa7e95cfff6b656d596a1efd24f267ffcef5a
dboatengg <dicksonboateng67@gmail.com>
2024-04-10 14:23:46 +0000

    Finish blog section

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -2,38 +2,27 @@ import { Box, Button, Flex, Heading, Image, Text } from "@chakra-ui/react"
 import blog1 from '../assets/blog1.png'
 import blog2 from '../assets/blog2.png'
 import blog3 from '../assets/blog3.png'
-// import { ArrowForwardIcon } from "@chakra-ui/icons"
 import { HiMiniArrowLongRight } from "react-icons/hi2"
+import blog from "../data/blog"
 
 const Blog = () => {
   return (
-    <Box padding="40px">
-        <Flex flexDirection="column" gap="10px" maxWidth="60%" textAlign='center' mx="auto" marginBottom="20px">
+    <Box paddingY="40px">
+        <Flex flexDirection="column" gap="10px" maxWidth={{md:"60%"}} textAlign='center' mx="auto" marginBottom="20px">
             <Heading>Caring is the new marketing</Heading>
             <Text>The Nextcent blog is the best place to read about the latest membership insights, trends and more. See who's joining the community, read about how our community are increasing their membership income and lot's more.</Text>
         </Flex>
-        <Flex flexDirection={{base:'column',md:'row'}} justifyContent="space-between" gap="30px" alignItems="center">
-            <Box>
-                <Image src={blog1}/>
-                <Box bg="silver" textAlign="center" paddingY="20px" paddingX="30px" maxWidth="300px" boxShadow="md">
-                    <Text>Creating Streamlined Safeguarding Processes with OneRen</Text>
-                    <Button variant='secondary' rightIcon={<HiMiniArrowLongRight />}>Read more</Button>
+        <Flex flexDirection={{base:'column',md:'row'}} gap={{base:'30px', md:'20px'}} alignItems="stretch">
+            {blog.map(data=>(
+            <Box bg="silver" key={data.id} minHeight="200px" display="flex" flexDirection="column" justifyContent="space-between">
+                <Image src={data.id === 1 ? blog1: data.id===2 ? blog2: data.id === 3 ? blog3:''}/>
+                <Box textAlign="center" paddingY="20px" paddingX="30px" boxShadow="md">
+                    <Text>{data.text}</Text>
+                    <Button variant='secondary' rightIcon={<HiMiniArrowLongRight />}>{data.btnText}</Button>
                 </Box>
             </Box>
-            <Box>
-                <Image src={blog2}/>
-                <Box>
-                    <Text>Creating Streamlined Safeguarding Processes with OneRen</Text>
-                    <Button variant='secondary' rightIcon={<HiMiniArrowLongRight />}>Read more</Button>
-                </Box>
-            </Box>
-            <Box >
-                <Image src={blog3}/>
-                <Box>
-                    <Text>Creating Streamlined Safeguarding Processes with OneRen</Text>
-                    <Button variant='secondary' rightIcon={<HiMiniArrowLongRight />}>Read more</Button>
-                </Box>
-            </Box>
+
+            ))}
         </Flex>
     </Box>
   )
---
+++ src/data/blog.ts
@@ -0,0 +1,26 @@
+export interface Blog {
+    id:number;
+    text: string;
+    btnText:string
+  }
+  
+  const blog: Blog[] = [
+    {
+        id:1,
+      text: "Creating Streamlined Safeguarding Processes with OneRen",
+      btnText:'Read more'
+    },
+    {
+        id:2,
+      text: "What are your safeguarding responsibilities and how can you manage them?",
+      btnText:'Read more'
+    },
+    {
+        id:3,
+      text: "Revamping the Membership Model with Triathlon Australia",
+      btnText:'Read more'
+    }
+  ];
+  
+  export default blog;
+  
