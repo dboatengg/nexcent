1c9d474262ea96faa968dfa44fa4f318fe0c4503
dboatengg <dicksonboateng67@gmail.com>
2024-04-10 14:34:25 +0000

    Clean up UI for Cta

--- src/components/Cta.tsx
+++ src/components/Cta.tsx
@@ -5,11 +5,13 @@ import { HiMiniArrowLongRight } from "react-icons/hi2"
 
 const Cta = () => (
   <Box bg="silver" paddingY="30px" marginTop="15px">
-    <Flex flexDirection="column" gap="30px" alignItems='center' maxWidth='35%' mx="auto">
-      <Heading textAlign='center'>Pellentesque suscipit fringilla libero eu.</Heading>
-      <Button rightIcon={<HiMiniArrowLongRight/>} variant='primary'>Get a Demo</Button>
-    </Flex>
+    <Box p="20px" maxWidth="1200px" mx="auto">
+      <Flex flexDirection="column" gap="30px" alignItems='center' maxWidth={{md:'50%'}} mx="auto">
+        <Heading fontSize={{base:'32px',md:'50px'}} textAlign='center'>Pellentesque suscipit fringilla libero eu.</Heading>
+        <Button rightIcon={<HiMiniArrowLongRight/>} variant='primary'>Get a Demo</Button>
+      </Flex>
+    </Box>
   </Box>
 )
 
-export default Cta
+export default Cta;
